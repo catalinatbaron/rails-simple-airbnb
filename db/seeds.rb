bef46f33fb8874e7d8e3b7e855c7ed226bc5ed87
catalina baron <catalinatbaron@gmail.com>
2019-02-13 11:04:12 +0200

    not finished yet, need to stylize the cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.delete_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: 'London',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Explore Old Barcelona from a Loft-Style Studio',
+  address: 'Barcelona',
+  description: 'Enjoy open-plan, loft-style living with neutral decor, open shelving, rustic exposed-beam ceilings. ',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Tropical Canggu Villa on Ricefield near the Beach',
+  address: 'Bali',
+  description: 'Lounge poolside out on the sunny patio of this lush, secluded villa with streamlined interiors and rich wood furnishings.',
+  price_per_night: 100,
+  number_of_guests: 2
+)
